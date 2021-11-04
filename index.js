b23579422cc3613aa4792241a782e5efdb0d8a2a
EJ Brown <ejbrown3c@gmail.com>
2021-11-04 19:07:53 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-function addingEventListener() {
+// const input = document.getElementById('input');
+
+// function addingEventListener() { 
+//     alert ('I was clicked');
+// }
+
+// input.addEventListener('click', addingEventListener);
+
+// input.addEventListener('click', function addingEventListerner () {
+//     alert('I was clicked')
+// });
+   
+function addingEventListener () {
+    const input = document.getElementById('input')
+    input.addEventListener('click', function (){
+    alert ('I was clicked')
+    });
 }
